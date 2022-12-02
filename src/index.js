2522d9308f79455b325a0ae51eace72e44484d99
Thomas Conger <t.e.conger@gmail.com>
2022-12-02 10:19:00 -0800

    added canvas resize for hero animation

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 console.log("Source JS file is loaded")
 
+// Hero Animation
+
+const heroCanvas = document.getElementById("hero-canvas")
+
+      // Set canvas width
+
+function resizeHero () {
+  heroCanvas.width = window.innerWidth
+  heroCanvas.height = window.innerHeight
+}
+
+resizeHero()
+
+window.addEventListener("resize", resizeHero)
+
+
+
+
+const ctx = heroCanvas.getContext('2d')
+
+ctx.fillRect(50,50,10,10)
+
+// Data Viz
