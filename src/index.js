85ef6d167e4d2328f1dee045ce378b1d68f10f98
Thomas Conger <t.e.conger@gmail.com>
2022-12-06 02:56:10 -0800

    eod commit. data import works. need to make charts responsive again

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ console.log("Source JS file is loaded")
 import {Market} from './scripts/hero_animation/market.js';
 import {scatterPlot} from './scripts/charts/scatter-plot';
 import {iconArray} from './scripts/charts/icon-array'
+import { csv } from 'd3';
 
 // Hero Animation
 
@@ -30,7 +31,7 @@ let margin = {
   top: 25,
   right: 25,
   bottom: 25,
-  left: 25
+  left: 100
 }
 
 
@@ -45,55 +46,28 @@ let data = d3.range(100).map((i) => {
 })
 
 // create an svg element and set its width and height to that of the window. by not setting a view box, the position and scale of the svg is set to the default
-const svg = d3.select('figure').append('svg')
+const svg = d3.select('#chart1').append('svg')
   .attr('style','background-color: black')
 
-  //  .attr('width', "inherit")
+  console.log(svg)
 
-// now that we have a canvas to render our data on, we need to make sure that when we render the data it actually fits! D3 lets you easily create functions to transform data.
-// The domain takes in an array that specifies the min and max values of the data that you want displayed. The range specifies the min and max values of the svg. The scale function translates, according to a set proportion, the original domain value to the new range so that all of the data fits on the graph AND remains accurate.
+const createCharts = async () => {
+  const data = await csv("./src/data/liquid-assets.csv")
 
-const y = d3.scaleLinear()
-  .domain([
-    0,
-    d3.max(data, (d) => d.y)
-  ])
-  .range([
-    svg.node().getBoundingClientRect().height - margin.bottom,
-    margin.top
-  ])
+  const plot = scatterPlot()
+    .width(950)
+    .height(500)
+    .data(data)
+    .margin(margin)
+    .radius(5)
+    .xValue((d) => +d.old_share)
+    .yValue((d) => +d.old_value)
 
-const x = d3.scaleLinear()
-  .domain(d3.extent(data, d => d.x))
-  .range([
-    margin.left,
-    svg.node().getBoundingClientRect().width - margin.right
-  ])
+  svg.call(plot)
 
-// marks are created by translating the original data according to scales
 
-let marks = data.map(d => ({
-  x: x(d.x),
-  y: y(d.y)
-}))
 
-const bar = svg.selectAll('rect')
-  .data(marks)
-  .join('rect')
-  .attr('width', 8)
-  .attr('height', 8)
-  .attr('x', (d) => d.x )
-  .attr('y', (d) => d.y )
-  .attr('fill', 'steelblue')
 
-const leftAxis = svg.append('g')
-.attr('transform', `translate(${margin.left}, 0)`)
-.attr('color', '#fff')
-.call(d3.axisLeft(y))
+}
 
-let height = svg.node().getBoundingClientRect().height
-
-const bottomAxis = svg.append('g')
-  .attr('transform', `translate(0,${height - margin.bottom })`)
-  .attr('color', '#fff')
-  .call(d3.axisBottom(x))
+createCharts()
--- src/scripts/charts/scatter-plot.js
+++ src/scripts/charts/scatter-plot.js
@@ -7,27 +7,63 @@ import {
 
 export const scatterPlot = () => {
 
-  //widht
-  //height
-  //data
-  //scalex
-  //scaley
-  //radius
-
   let width;
   let height;
   let data;
-  let scaleX;
-  let scaleY;
+  let xValue;
+  let yValue;
+  let margin;
   let radius;
+  let cohort;
 
   const my = (selection) => {
+
     // create scale x function
-    // create scale y function
+    const xScale = scaleLinear()
+      .domain(extent(data, xValue))
+      .range([margin.left, width - margin.right]);
+
+      // create scale y function
+
+    const yScale = scaleLinear()
+      .domain(extent(data, yValue))
+      .range([height - margin.bottom, margin.top]);
+
     // create marks based on scaled data
-    // render marks 
-    // create x axis
+
+    const marks = data.map((d) => ({
+      x: xScale(xValue(d)),
+      y: yScale(yValue(d))
+    }));
+
+    // console.log(marks)
+
+    // render marks
+
+    const circles = selection.selectAll('circle')
+      .data(marks)
+      .join('circle')
+      .attr('fill', 'blue')
+      .attr('cx', (d) => d.x)
+      .attr('cy', (d) => d.y)
+      .attr('r', (d) => radius);
+
     // create y axis
+
+    selection
+      .append('g')
+      .attr('class', 'y-axis')
+      .attr('transform', `translate(${margin.left}, 0)` )
+      .call(axisLeft(yScale));
+
+
+
+    // create  axis
+    selection
+      .append('g')
+      .attr('class', 'x-axis')
+      .attr('transform', `translate(0, ${height - margin.bottom})` )
+      .call(axisBottom(xScale))
   }
 
   my.width = function (_) {
@@ -36,96 +72,26 @@ export const scatterPlot = () => {
   my.height = function (_) {
     return arguments.length ? (height = _, my) : height
   }
+  my.margin = function (_) {
+    return arguments.length ? (margin = _, my) : margin
+  }
   my.data = function (_) {
     return arguments.length ? (data = _, my) : data
   }
-  my.scaleX = function (_) {
-    return arguments.length ? (scaleX = _, my) : scaleX
+  my.xValue = function (_) {
+    return arguments.length ? (xValue = _, my) : xValue
   }
-  my.scaleY = function (_) {
-    return arguments.length ? (scaleY = _, my) : scaleY
+  my.yValue = function (_) {
+    return arguments.length ? (yValue = _, my) : yValue
   }
 
   my.radius = function (_) {
     return arguments.length ? (radius = _, my) : radius
   }
+  my.cohort = function (_) {
+    return arguments.length ? (cohort = _, my) : cohort
+  }
 
-
-
- // the above function is a wrapper that returns an uninvoked function
-
-
-
+  return my
 
 }
-
-
-// const data = []
-
-// d3.csv("../src/data/liquid_assets.csv", d3.parseRow).then((d) => {
-//   console.log(d);
-//   console.log(d[0]);
-// });
-
-
-
-
-
-// create an svg element and set its width and height to that of the window. by not setting a view box, the position and scale of the svg is set to the default
-// const svg = d3.select('figure').append('svg')
-//   .attr('style','background-color: black')
-
-  //  .attr('width', "inherit")
-
-// now that we have a canvas to render our data on, we need to make sure that when we render the data it actually fits! D3 lets you easily create functions to transform data.
-// The domain takes in an array that specifies the min and max values of the data that you want displayed. The range specifies the min and max values of the svg. The scale function translates, according to a set proportion, the original domain value to the new range so that all of the data fits on the graph AND remains accurate.
-
-// const y = d3.scaleLinear()
-//   .domain([
-//     0,
-//     d3.max(data, (d) => console.log(d[0]))
-//   ])
-//   .range([
-//     svg.node().getBoundingClientRect().height - margin.bottom,
-//     margin.top
-//   ])
-
-// const x = d3.scaleLinear()
-//   .domain(d3.extent(data, d => d.now))
-//   .range([
-//     margin.left,
-//     svg.node().getBoundingClientRect().width - margin.right
-//   ])
-
-
-
-// marks are created by translating the original data according to scales
-
-// let marks = data.map(d => (
-//   {
-//   x: x(d.now),
-//   y: y(d.old)
-// }))
-
-// console.log(data[1].now)
-
-// const bar = svg.selectAll('rect')
-//   .data(marks)
-//   .join('rect')
-//   .attr('width', 8)
-//   .attr('height', 8)
-//   .attr('x', (d) => d.x )
-//   .attr('y', (d) => d.y )
-//   .attr('fill', 'steelblue')
-
-// const leftAxis = svg.append('g')
-// .attr('transform', `translate(${margin.left}, 0)`)
-// .attr('color', '#fff')
-// .call(d3.axisLeft(y))
-
-// let height = svg.node().getBoundingClientRect().height
-
-// const bottomAxis = svg.append('g')
-//   .attr('transform', `translate(0,${height - margin.bottom })`)
-//   .attr('color', '#fff')
-//   .call(d3.axisBottom(x))
